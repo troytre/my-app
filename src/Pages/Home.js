1cb81f095b3efffe1268138d4aed0bd45e2f9028
Ranjan45-React <phpranjan5@gmail.com>
2024-03-14 18:56:08 +0530

    Added API Requests

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,97 +1,82 @@
 
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function Home() {
-    const ContentContainer = styled.div`
-  width:auto;
-  min-height:500px;`
-    const Banner = styled.div`
-  width:auto;
-  height:400px;
-  margin-bottom:20px;
-  img {
-    width:100%;
-    height:100%;
-  }
-  `
-    const CardContainer = styled.div`
-  display:flex;
-  flex-wrap:wrap;
-  justify-content:center;
-  align-items:center;
-  gap:10px;
-  `
+  const [photos, setPhotos] = useState([])
+  const start = 0;
+  const limit = 10;
+  const apiUrl = `https://jsonplaceholder.typicode.com/photos?_start=${start}&_limit=${limit}`
 
+  const fetchData = async () => {
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    setPhotos(data)
+  }
 
-    const Card = styled.div`
-  padding:10px;
-  display:flex;
-  flex-direction:column;
-  width:350px;
-  height:300px;
-  background:#ccc;
-  img {
-    width:auto;
-    height:150px;
-  }
-  a{
-    color:red;
-    font-size:20px;
-  }
-  p{
-    font-size:20px;
-  }
-  `
-    return (
-        <ContentContainer>
-            <Banner>
-                <img src="./banner.jpg" alt="Banner Here" />
-            </Banner>
-            <CardContainer>
-                <Card>
-                    <img src="./lob.png" alt="Image here" />
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to </p>
-                    <a>Read More...</a>
-                </Card>
-                <Card>
-                    <img src="./lob.png" alt="Image here" />
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to </p>
-                    <a>Read More...</a>
-                </Card>
-                <Card>
-                    <img src="./lob.png" alt="Image here" />
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to </p>
-                    <a>Read More...</a>
-                </Card>
-                <Card>
-                    <img src="./lob.png" alt="Image here" />
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to </p>
-                    <a>Read More...</a>
-                </Card>
-                <Card>
-                    <img src="./lob.png" alt="Image here" />
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to </p>
-                    <a>Read More...</a>
-                </Card>
-                <Card>
-                    <img src="./lob.png" alt="Image here" />
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to </p>
-                    <a>Read More...</a>
-                </Card>
-                <Card>
-                    <img src="./lob.png" alt="Image here" />
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to </p>
-                    <a>Read More...</a>
-                </Card>
-                <Card>
-                    <img src="./lob.png" alt="Image here" />
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to </p>
-                    <a>Read More...</a>
-                </Card>
+  const getCardData = () => (
+    photos.map((photo) => (
+      <Card>
+        <img src={"../yeh-jawaani-hai-deewani-1-one_one.webp"} alt="Image here" />
+        <p>{photo.title}</p>
+        <a href={photo.url} target="_blank">Read More...</a>
+      </Card>
+    ))
+  )
 
-            </CardContainer>
-        </ContentContainer>
-    );
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <ContentContainer>
+      <Banner>
+        <img src="./banner.jpg" alt="Banner Here" />
+      </Banner>
+      <CardContainer>
+        {getCardData()}
+      </CardContainer>
+    </ContentContainer>
+  );
 }
+const ContentContainer = styled.div`
+width:auto;
+min-height:500px;`
+const Banner = styled.div`
+width:auto;
+height:400px;
+margin-bottom:20px;
+img {
+  width:100%;
+  height:100%;
+}
+`
+const CardContainer = styled.div`
+display:flex;
+flex-wrap:wrap;
+justify-content:center;
+align-items:center;
+gap:10px;
+`
 
+
+const Card = styled.div`
+padding:10px;
+display:flex;
+flex-direction:column;
+width:350px;
+height:300px;
+background:#ccc;
+img {
+  width:auto;
+  height:150px;
+}
+a{
+  color:red;
+  font-size:20px;
+}
+p{
+  font-size:20px;
+}
+`
 export default Home;
